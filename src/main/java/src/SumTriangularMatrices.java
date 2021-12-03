82b41a3257b3c5b19a99f1c710a4046b7391967a
Jamilu Salisu <jamilusalis@gmail.com>
2021-12-03 07:27:41 +0100

    add spacing

--- src/main/java/src/SumTriangularMatrices.java
+++ src/main/java/src/SumTriangularMatrices.java
@@ -8,7 +8,6 @@ class SumTriangularMatrices {
     public static void main (String [] agrs) {
         int array_size = 6;
         
-        
         int[][] lower_matrix = {
             {60}, 
             {61, 62}, 
@@ -29,33 +28,34 @@ class SumTriangularMatrices {
         };
         
         System.out.println("Lower Triangular Matrix:");
-        for (int i=0;i<lower_matrix.length;i++) {
-            for (int j=0;j<lower_matrix[i].length;j++) {
-                System.out.print(lower_matrix[i][j] + " ");
+        for (int[] lower : lower_matrix) {
+            for (int i = 0; i < lower.length; i++) {
+                System.out.print(lower[i] + " ");
             }
             System.out.println();
         }
         
         System.out.println("\nUpper Triangular Matrix");
-        for (int[] upper_matrix1 : upper_matrix) {
-            for (int j = 0; j < upper_matrix1.length; j++) {
-                System.out.print(upper_matrix1[j] + " ");
+        for (int[] upper : upper_matrix) {
+            for (int i = 0; i < upper.length; i++) {
+                System.out.print(upper[i] + " ");
             }
             System.out.println();
         }
         
         System.out.println("\nSum of Upper and Lower of a Triangular Matrix:");
-        for (int i=0;i<array_size;i++) {
-            int L_point = lower_matrix[i][lower_matrix[i].length-1];
-            int U_point = upper_matrix[i][0];
-            for (int j=0;j<lower_matrix[i].length;j++) {
-                if (j==(lower_matrix[i].length)-1) {
-                    System.out.print(L_point + U_point + " ");
+        for (int i = 0; i < array_size; i++) {
+            int lower_diagonal = lower_matrix[i][lower_matrix[i].length-1];
+            int upper_diagonal = upper_matrix[i][0];
+            
+            for (int j = 0; j < lower_matrix[i].length; j++) {
+                if (j == (lower_matrix[i].length)-1) {
+                    System.out.print(lower_diagonal + upper_diagonal + " ");
                     break;
                 }
                 System.out.print(lower_matrix[i][j] + " ");
             }
-            for (int k=1;k<upper_matrix[i].length;k++) {
+            for (int k = 1; k <upper_matrix[i].length; k++) {
                 System.out.print(upper_matrix[i][k] + " ");
             }
             System.out.println();
